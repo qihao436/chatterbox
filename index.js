6f17bfd07b34cd67692f4559f602facfc2c15e75
qihao436 <qihao436@hotmail.com>
2017-06-10 11:19:41 -0400

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs =require('fs');
+var extract = require('./extractFilePath');
+var wss = require('./websockets-server');
+
+var handleError = function(err,res){
+  res.writeHead(404);
+  res.end();
+}
+
+var server = http.createServer(function(req,res){
+  console.log('Responding to a requret.');
+
+  var filePath = extract(req.url);
+
+  fs.readFile(filePath, function(err,data){
+    if(err){
+      handleError(err,res)
+    } else{
+      res.end(data);
+    }
+  });
+});
+server.listen(3000);
